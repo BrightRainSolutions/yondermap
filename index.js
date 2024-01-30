855ce0667ea69859d245e2a047d06d9cfc457b20
David Puckett <david@brightrain.com>
2024-01-29 17:02:31 -0700

    moar work on horse(not yet giddying up so commented out for now), switch to browser-sync for hot dev reload, remove maptiks (they dead)

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ new Vue({
         if (queryString !== "") {
             // if we have url params get them
             const urlParams = new URLSearchParams(queryString);
-            // by convention we are using 'origin' and 'destination'
+            // by convention we are using 'start' and 'end'
             const start = urlParams.get('start');
             const end = urlParams.get('end');
             // if we got them then set corresponding local data and the route will
@@ -92,7 +92,6 @@ new Vue({
                 //trackEvent(category, action, [name], [value])
                 _paq.push(['trackEvent', 'Route', 'FromURL']);
             }
-            
         } 
     },
     data: {
@@ -101,6 +100,7 @@ new Vue({
                 instructions: "",
                 icon: "",
                 location: [],
+                bearing: "",
                 selected: false
             }
         ],
@@ -117,6 +117,21 @@ new Vue({
             "type": "Feature",
             "properties": {}
         },
+        // for the horse model
+        // the mapbox custom later that contains the horse model
+        thatHorseLayer: null,
+        modelAsMercatorCoordinate: null,
+        modelTransform: null,
+        modelX: -105.5,
+        modelY: 40,
+        modelRotate: [],
+        mesh: null,
+        mixer: null,
+        camera: null,
+        scene: null,
+        theta: 0,
+        prevTime: Date.now(),
+        // core mapbox directions component
         directionsThing: null,
         origin: "",
         destination: "",
@@ -308,26 +323,30 @@ new Vue({
             this.map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/brightrain/cjrmcme6m0kwp2sjz5qvez9ga',
-                center: [-110, 40],
-                zoom: 3,
-                maptiks_id: "yondermap",
+                center: [-96, 45],
+                // ToDo: for the horse
+                //center: [this.modelX, this.modelY],
+                zoom: 4,
+                pitch: 10,
+                bearing: 0
             });
 
             this.directionsThing = new MapboxDirections({
                 accessToken: mapboxgl.accessToken,
                 steps: false,
-                geometries: 'polyline',
                 controls: {instructions: false, profileSwitcher: false},
                 placeholderOrigin: 'where y`all startin from?',
                 placeholderDestination: 'where y`all wanna go?',
                 styles: this.directionsStyleOptions
             });
-            
             this.map.addControl(this.directionsThing, 'top-right');
+
+            // ToDo: the horse is hard, wip
+            //this.setupThatHorse();
 
             // when the map is loaded setup our route event
             this.map.on('load', () => {
-                // Listen for the `directions.route` event then populate our directions panel
+              // Listen for the `directions.route` event then populate our directions panel
                 this.directionsThing.on('route', ev => {
                     _paq.push(['trackEvent', 'Route', 'RouteRequested']);
                     let originCoords = this.directionsThing.getOrigin().geometry.coordinates;
@@ -365,7 +384,12 @@ new Vue({
                         }
                     });
                 });
-            });            
+            });
+            
+            this.map.on('zoom', () => {
+              let zoom = this.map.getZoom().toFixed(2);
+              //this.scaleThatHorse(zoom);
+          });
         },
         yonderThatRoute(ev) {
             // clear existing steps
@@ -379,8 +403,12 @@ new Vue({
             });
 
             // place our step marker at the first step
-            this.locationData.geometry.coordinates = steps[0].maneuver.location;
+            let thatFirstManeuver = steps[0].maneuver;
+            this.locationData.geometry.coordinates = thatFirstManeuver.location;
             this.map.getSource('step-location-source').setData(this.locationData);
+            // place and scale that horse
+            //this.giddyUpHorse(thatFirstManeuver.location[0], thatFirstManeuver.location[1], thatFirstManeuver.bearing_after);
+            //this.scaleThatHorse(this.map.getZoom().toFixed(2));
             
             // iterate steps from the route to create our steps
             // for direction panel
@@ -392,12 +420,14 @@ new Vue({
                 // yonder up them words
                 let themWords = this.swapOutThemWords(instructions);
                 let iconPath = this.getThatIcon(instructions);
-
+                // create a step with only what we need to push it in our data
+                // including the bearing after the manuever so we can point that horse the right way
                 this.steps.push(
                     {
                         instructions: themWords,
                         icon: iconPath,
                         location: location,
+                        bearing: step.maneuver.bearing_after,
                         selected: false
                     }
                 );
@@ -423,7 +453,6 @@ new Vue({
                 .replace(/Head/g, "Yonder")
                 .replace(/Turn left/g, "Hang a Louie")
                 .replace(/Turn right/g, "Hang a Ralph")
-                .replace(/Head/g, "Yonder")
                 .replace(/Take a slight/g, "Mosey on over to the")
                 .replace(/take a slight/g, "mosey on over to the")
                 .replace(/Take/g, "Giddy up on")
@@ -468,8 +497,8 @@ new Vue({
             }
             return iconPath;
         },
-        goTo(step) {
-            this.steps.forEach(step => { step.selected=false; });
+        goTo(step) {  
+          this.steps.forEach(step => { step.selected=false; });
             step.selected = true;
             //  [-105.096323, 40.587368]
             this.map.flyTo({
@@ -484,6 +513,82 @@ new Vue({
             // place the marker at the step location
             this.locationData.geometry.coordinates = step.location;
             this.map.getSource('step-location-source').setData(this.locationData);
+            // this horse is wip...
+            //this.giddyUpHorse(step.location[0], step.location[1], step.bearing);
+            //console.log("manuever bearing " + step.bearing);
+            //this.map.getSource('step-location-source').setData(this.locationData);
+        },
+        giddyUpHorse(x, y, heading) {
+          this.modelX = x;
+          this.modelY = y;
+          this.modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+              [this.modelX, this.modelY],
+              0
+          );
+  
+          // transformation parameters to position, rotate and scale the 3D model onto the map
+          
+          this.modelTransform = {
+              translateX: this.modelAsMercatorCoordinate.x,
+              translateY: this.modelAsMercatorCoordinate.y,
+              translateZ: this.modelAsMercatorCoordinate.z,
+              rotateX: this.modelRotate[0],
+              //rotateY: (heading - 90),
+              rotateY: this.modelRotate[1],
+              rotateZ: this.modelRotate[2],
+              /* Since our 3D model is in real world meters, a scale transform needs to be
+              * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+              */
+              scale: this.modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+          };
+
+          this.mesh.rotation.y = THREE.Math.degToRad(heading);
+        },
+        scaleThatHorse(zoom) {
+          let scaleFactor = 1;
+              if(zoom >= 13) {
+                  scaleFactor = 2;
+              }
+              else if(zoom >= 12 && zoom < 13) {
+                  scaleFactor = 12;
+              }
+              else if(zoom >= 11 && zoom < 12) {
+                  scaleFactor = 16;
+              }
+              else if(zoom >= 10 && zoom < 11) {
+                  scaleFactor = 25;
+              }
+              else if(zoom >= 9 && zoom < 10) {
+                  scaleFactor = 50;
+              }
+              else if(zoom >= 8 && zoom < 9) {
+                  scaleFactor = 70;
+              }
+              else if(zoom >= 7 && zoom < 8) {
+                  scaleFactor = 90;
+              }
+              else if(zoom >= 8 && zoom < 9) {
+                  scaleFactor = 120;
+              }
+              else if(zoom >= 7 && zoom < 8) {
+                  scaleFactor = 150;
+              }
+              else if(zoom >= 6 && zoom < 7) {
+                  scaleFactor = 200;
+              }
+              else if(zoom >= 5 && zoom < 6) {
+                  scaleFactor = 250;
+              }
+              else if(zoom >= 4 && zoom < 5) {
+                  scaleFactor = 350;
+              }
+              else if(zoom >= 3 && zoom < 4) {
+                  scaleFactor = 500;
+              }
+              else {
+                  scaleFactor = 600;
+              }
+              this.mesh.scale.set(scaleFactor, scaleFactor, scaleFactor);
         },
         getLink() {
             if(this.steps.length > 1) {
@@ -508,34 +613,162 @@ new Vue({
             this.notify = true;
             setTimeout(() => { this.notify = false; }, 3000);
             _paq.push(['trackEvent', 'Route', 'CopyRouteLink']);
-          },
-          findEm() {
-            _paq.push(['trackEvent', 'Locate', 'LocateBtn']);
-            navigator.geolocation.getCurrentPosition(position => {
-                const latitude  = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                
-                // place the marker at the location
-                this.locationData.geometry.coordinates = [longitude, latitude];
-                this.map.getSource('step-location-source').setData(this.locationData);
+        },
+        findEm() {
+          _paq.push(['trackEvent', 'Locate', 'LocateBtn']);
+          navigator.geolocation.getCurrentPosition(position => {
+              const latitude  = position.coords.latitude;
+              const longitude = position.coords.longitude;
+              
+              // place the marker at the location
+              this.locationData.geometry.coordinates = [longitude, latitude];
+              //this.map.getSource('step-location-source').setData(this.locationData);
+              //this.giddyUpHorse(longitude, latitude, 0);
 
-                // set it as the origin of a route
-                this.directionsThing.setOrigin([longitude, latitude]);
+              // set it as the origin of a route
+              this.directionsThing.setOrigin([longitude, latitude]);
 
-                this.map.flyTo({
-                    center: [longitude, latitude],
-                    zoom: 14,
-                    speed: 1.5,
-                    curve: 1,
-                    easing(t) {
-                      return t;
-                    }
-                });
-            });
-          },
-          handlePanelMin() {
-            this.minPanel = !this.minPanel;
-            document.getElementById("panel").classList.toggle("min-panel");
-          }
+              this.map.flyTo({
+                  center: [longitude, latitude],
+                  zoom: 14,
+                  speed: 1.5,
+                  curve: 1,
+                  easing(t) {
+                    return t;
+                  }
+              });
+          });
+        },
+        handlePanelMin() {
+          this.minPanel = !this.minPanel;
+          document.getElementById("panel").classList.toggle("min-panel");
+        },
+        setupThatHorse() {
+          // parameters to ensure the model is georeferenced correctly on the map
+          // and that our horse is actually standing on the ground via PI !
+          this.modelRotate = [Math.PI / 2, 0, 0];
+          this.modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat([this.modelX, this.modelY], 0);
+
+          // transformation parameters to position, rotate and scale the 3D model onto the map
+          this.modelTransform = {
+              translateX: this.modelAsMercatorCoordinate.x,
+              translateY: this.modelAsMercatorCoordinate.y,
+              translateZ: this.modelAsMercatorCoordinate.z,
+              rotateX: this.modelRotate[0],
+              rotateY: this.modelRotate[1],
+              rotateZ: this.modelRotate[2],
+              /* Since our 3D model is in real world meters, a scale transform needs to be
+              * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+              */
+              scale: this.modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+          };
+
+          var THREE = window.THREE;
+          
+          // configuration of the custom layer for a 3D model per the CustomLayerInterface
+          this.thatHorseLayer = {
+              id: '3d-model',
+              type: 'custom',
+              renderingMode: '3d',
+              onAdd: (map, gl) => {
+                  this.camera = new THREE.Camera();
+                  this.scene = new THREE.Scene();
+
+                  // create two three.js lights to illuminate the model
+                  var directionalLight = new THREE.DirectionalLight(0xffffff);
+                  directionalLight.position.set(0, -70, 100).normalize();
+                  this.scene.add(directionalLight);
+
+                  var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+                  directionalLight2.position.set(0, 70, 100).normalize();
+                  this.scene.add(directionalLight2);
+
+                  let yonderVue = this;
+                  // use the three.js GLTF loader to add the 3D model to the three.js scene
+                  var loader = new THREE.GLTFLoader();
+                  loader.load('https://assets.codepen.io/1053746/horse.glb',
+                      function (gltf) {
+                        yonderVue.scene.add(gltf.scene);
+                        yonderVue.mesh = gltf.scene.children[0];
+                        yonderVue.mesh.scale.set(500, 500, 500);
+                        yonderVue.scene.add(yonderVue.mesh);
+
+                        yonderVue.mixer = new THREE.AnimationMixer(yonderVue.mesh);
+
+                        yonderVue.mixer
+                          .clipAction(gltf.animations[0])
+                          .setDuration(1).play();
+                        yonderVue.scaleThatHorse(yonderVue.map.getZoom().toFixed(2));
+                      }.bind(yonderVue.thatHorseLayer)
+                  );
+
+                  // use the Mapbox GL JS map canvas for three.js
+                  this.thatHorseLayer.renderer = new THREE.WebGLRenderer({
+                      canvas: this.map.getCanvas(),
+                      context: gl,
+                      antialias: true
+                  });
+
+                  this.thatHorseLayer.renderer.autoClear = false;
+              },
+              // method fired on each animation frame
+              // https://docs.mapbox.com/mapbox-gl-js/api/#map.event:render
+              render: (gl, matrix) => {
+                  var rotationX = new THREE.Matrix4().makeRotationAxis(
+                      new THREE.Vector3(1, 0, 0),
+                      this.modelTransform.rotateX
+                  );
+                  var rotationY = new THREE.Matrix4().makeRotationAxis(
+                      new THREE.Vector3(0, 1, 0),
+                      this.modelTransform.rotateY
+                  );
+                  var rotationZ = new THREE.Matrix4().makeRotationAxis(
+                      new THREE.Vector3(0, 0, 1),
+                      this.modelTransform.rotateZ
+                  );
+
+                  var m = new THREE.Matrix4().fromArray(matrix);
+
+                  this.modelTransform.scale = this.modelAsMercatorCoordinate.meterInMercatorCoordinateUnits();
+
+                  var l = new THREE.Matrix4()
+                      .makeTranslation(
+                        this.modelTransform.translateX,
+                        this.modelTransform.translateY,
+                        this.modelTransform.translateZ
+                      )
+                      .scale(
+                          new THREE.Vector3(
+                            this.modelTransform.scale,
+                            -this.modelTransform.scale,
+                            this.modelTransform.scale
+                          )
+                      )
+                      .multiply(rotationX)
+                      .multiply(rotationY)
+                      .multiply(rotationZ);
+
+                  this.camera.projectionMatrix = m.multiply(l);
+                  this.thatHorseLayer.renderer.resetState();
+                  this.thatHorseLayer.renderer.render(this.scene, this.camera);
+                  this.map.triggerRepaint();
+                  this.theta += 0.1;
+                  this.camera.position.x = Math.sin( THREE.MathUtils.degToRad( this.theta ) );
+                  this.camera.position.z = Math.cos( THREE.MathUtils.degToRad( 500 ) );
+                  this.camera.lookAt( 0, 0, 0 );
+                  if (this.mixer) {
+                      const time = Date.now();
+                      this.mixer.update( ( time - this.prevTime ) * 0.001 );
+                      this.prevTime = time;
+                  }
+              }
+          };
+
+          this.map.on('style.load', () => {
+              this.map.addLayer(this.thatHorseLayer);
+              // scale the horse by initial zoom
+              this.scaleThatHorse(this.map.getZoom().toFixed(2));
+          });
+        }
     }
 });
